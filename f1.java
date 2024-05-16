627910155107808cc9098197ed63772cea4aaa80
Tejaswini699 <teju000kamble@gmail.com>
2024-05-16 22:18:40 +0530

    Git commited

---
+++ f1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class f1
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Hello Everyone");
+    }
+}
---
+++ f2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class f2
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Hello Tejaswini");
+    }
+}
